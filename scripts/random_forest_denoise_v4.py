a63b7951f1db2d4e99804e8e2ea807e7c1d36969
carodak <carodak2@gmail.com>
2018-11-25 16:30:53 -0500

    csv-random-forest-51% test set

--- scripts/random_forest_denoise_v4.py
+++ scripts/random_forest_denoise_v4.py
@@ -14,11 +14,11 @@ parent_dir = dirname(dirname(abspath(__file__)))
 #Load pickle denoised training set
 images_train = np.array(pickle.load(open(parent_dir+"/data/train_cleaned_examples.p", "rb")))
 
-#print("Images %s \n Shape %s \n"%(images_train,images_train.shape))
+#Load pickle denoised testing set
+images_test = np.array(pickle.load(open(parent_dir+"/data/test_cleaned_examples.p", "rb")))
 
-
-#Normalize the pictures: 41x41
-#Add nb_row rows of 0 below and nb_colums colums of 0 at the right to have X of size 41x41
+#Normalize the pictures: 46x46
+#Add nb_row rows of 0 below and nb_colums colums of 0 at the right to have X of size 46x46
 
 n = len(images_train)
 n2 = images_train[0].shape
@@ -33,30 +33,37 @@ for i in range(n):
     X[i] = np.pad(images_train[i], ((0,nb_row),(0,nb_columns)), mode='constant', constant_values=0)
 
 #print(X)
+#Normalise pictures of test set too
+n_ = len(images_test)
+n2_ = images_test[0].shape
+size_ = 46
+X_= np.zeros([n_,size_,size_])
 
-#Test our model with the validation set
-images_train = X[0:8000,:]
-images_validation = X[8000:,:]
+for i in range(n_):
+    x_size = images_test[i].shape
+    nb_row = size_-x_size[0]
+    nb_columns = size_-x_size[1]
+    x = np.pad(images_test[i], ((0,nb_row),(0,nb_columns)), mode='constant', constant_values=0)
+    X_[i] = np.pad(images_test[i], ((0,nb_row),(0,nb_columns)), mode='constant', constant_values=0)
+
+#print(X_)
+
+images_train = X
+images_test = X_
 
 #Load labels for training
-train_labels = np.genfromtxt(parent_dir+'/data/train_labels.csv', names=True, delimiter=',', dtype=[('Id', 'i8'), ('Category', 'S5')])
+train_labels = np.genfromtxt(parent_dir+'/data/train_labels.csv',delimiter=',',dtype=None, names=True)
+print(train_labels)
 y = [x[1] for x in train_labels]
 
-#Load denoised test set
-images_test = np.array(pickle.load(open(parent_dir+"/data/test_cleaned_examples.p", "rb")))
-
 #We want matrixes of vectors
-images_train = images_train.reshape(8000,2116)
-images_validation = images_validation.reshape(2000,2116)
+images_train = images_train.reshape(10000,2116)
+images_test = images_test.reshape(10000,2116)
 
-train_labels = y[0:8000]
-validation_labels = y[8000:]
-
+train_labels = y
 
 clf = RandomForestClassifier(random_state=0, n_estimators=500, max_depth=15)
-#bclf = AdaBoostClassifier(base_estimator=clf,n_estimators=clf.n_estimators)
 
-#clf = GridSearchCV(estimator=rfc, param_grid=param_grid, cv= 5)
 clf.fit(images_train, train_labels)
 
 #print("best params: ", clf.best_params_)
@@ -68,19 +75,12 @@ print("Accuracy on the training set: %s"%acc)
 #cv1 = cross_val_score(clf, images_train, train_labels, scoring='recall_macro',cv=5)
 #print("Accuracy on the training set: %s \n %s \n"%(acc,cv1))
 
-acc2 = clf.score(images_validation, validation_labels)
-print("Accuracy on the test set: %s"%acc2)
-
-#Uncomment to use cross validation
-#cv2 = cross_val_score(clf, images_test, test_labels, scoring='recall_macro',cv=5)
-#print("Accuracy on the training set: %s \n %s \n"%(acc2,cv2))
-
 print("clf.feature_importances: %s \n "%clf.feature_importances_)
 
-#results = clf.predict(images_test)
+results = clf.predict(images_test)
 
-#print("Results (predictions on the test set): %s \n"%results)
+print("Results (predictions on the test set): %s \n"%results)
 
-#np.savetxt(parent_dir+'/results/test_random_forest_v2_denoise.csv', [p for p in results], delimiter=' ', fmt='%s')
+np.savetxt(parent_dir+'/results/test_random_forest_v4_denoise.csv', [p for p in results], delimiter=' ', fmt='%s')
 
-#print("Predictions as been saved as a csv file")
+print("Predictions as been saved as a csv file")
